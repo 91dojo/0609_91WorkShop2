5873cc726dd08ac789f77de6882c8b090baefa32
thirmoon@gmail.com <thirmoon@gmail.com>
2018-06-09 23:11:40 +0800

    add TestData

--- WorkShop2/WorkShop22/Class1Tests.cs
+++ WorkShop2/WorkShop22/Class1Tests.cs
@@ -37,41 +37,89 @@ namespace WorkShop2.Tests
         [TestMethod()]
         public void OneMonthPartialBudget()
         {
+            GiveBudgets(new List<Budget>()
+            {
+                new Budget() {YearMonth = "201802", Amount = 280},
+                new Budget() {YearMonth = "201806", Amount = 300},
+                new Budget() {YearMonth = "201807", Amount = 310}
+            });
             BudgetResultShouldBe(new DateTime(2018, 6, 1), new DateTime(2018, 6, 15), 150m);
         }
         [TestMethod()]
         public void TwoMonthBothNotBudget()
         {
+            GiveBudgets(new List<Budget>()
+            {
+                new Budget() {YearMonth = "201802", Amount = 280},
+                new Budget() {YearMonth = "201806", Amount = 300},
+                new Budget() {YearMonth = "201807", Amount = 310}
+            });
             BudgetResultShouldBe(new DateTime(2018, 3, 1), new DateTime(2018, 4, 2), 0m);
         }
         [TestMethod()]
         public void TwoMonth_StartMonthNoBudget_EndMonthHasBudget()
         {
+            GiveBudgets(new List<Budget>()
+            {
+                new Budget() {YearMonth = "201802", Amount = 280},
+                new Budget() {YearMonth = "201806", Amount = 300},
+                new Budget() {YearMonth = "201807", Amount = 310}
+            });
             BudgetResultShouldBe(new DateTime(2018, 5, 20), new DateTime(2018, 6, 10), 100m);
         }
         [TestMethod()]
         public void TwoMonth_StartMonthHasBudget_EndMonthNoBudget()
         {
+            GiveBudgets(new List<Budget>()
+            {
+                new Budget() {YearMonth = "201802", Amount = 280},
+                new Budget() {YearMonth = "201806", Amount = 300},
+                new Budget() {YearMonth = "201807", Amount = 310}
+            });
             BudgetResultShouldBe(new DateTime(2018, 7, 30), new DateTime(2018, 8, 22), 20m);
         }
         [TestMethod()]
         public void TwoMonthBothBudget()
         {
+            GiveBudgets(new List<Budget>()
+            {
+                new Budget() {YearMonth = "201802", Amount = 280},
+                new Budget() {YearMonth = "201806", Amount = 300},
+                new Budget() {YearMonth = "201807", Amount = 310}
+            });
             BudgetResultShouldBe(new DateTime(2018, 6, 15), new DateTime(2018, 7, 14), 300m);
         }
         [TestMethod()]
         public void OverYearBudget()
         {
+            GiveBudgets(new List<Budget>()
+            {
+                new Budget() {YearMonth = "201802", Amount = 280},
+                new Budget() {YearMonth = "201806", Amount = 300},
+                new Budget() {YearMonth = "201807", Amount = 310}
+            });
             BudgetResultShouldBe(new DateTime(2017, 12, 1), new DateTime(2018, 2, 1), 10m);
         }
         [TestMethod()]
         public void OverYearBudget_EndYearHas2Budget()
         {
+            GiveBudgets(new List<Budget>()
+            {
+                new Budget() {YearMonth = "201802", Amount = 280},
+                new Budget() {YearMonth = "201806", Amount = 300},
+                new Budget() {YearMonth = "201807", Amount = 310}
+            });
             BudgetResultShouldBe(new DateTime(2017, 11, 1), new DateTime(2018, 6, 30), 580m);
         }
         [TestMethod()]
         public void AllYearBudget()
         {
+            GiveBudgets(new List<Budget>()
+            {
+                new Budget() {YearMonth = "201802", Amount = 280},
+                new Budget() {YearMonth = "201806", Amount = 300},
+                new Budget() {YearMonth = "201807", Amount = 310}
+            });
             BudgetResultShouldBe(new DateTime(2018, 01, 1), new DateTime(2018, 12, 31), 890);
         }
 
@@ -79,6 +127,12 @@ namespace WorkShop2.Tests
         [TestMethod()]
         public void ThrowExpection()
         {
+            GiveBudgets(new List<Budget>()
+            {
+                new Budget() {YearMonth = "201802", Amount = 280},
+                new Budget() {YearMonth = "201806", Amount = 300},
+                new Budget() {YearMonth = "201807", Amount = 310}
+            });
             var result = _budgetCalculate.Result(new DateTime(2018, 5, 1), new DateTime(2018, 4, 30));
         }
         private void BudgetResultShouldBe(DateTime startTime, DateTime endTime, decimal expected)
