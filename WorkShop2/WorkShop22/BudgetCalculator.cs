5b469b53bd516db2fee68d4ab28c6d0e90770cc2
thirmoon@gmail.com <thirmoon@gmail.com>
2018-06-10 12:23:43 +0800

    change startTime and endTime to period startTime and endTime

--- WorkShop2/WorkShop22/BudgetCalculator.cs
+++ WorkShop2/WorkShop22/BudgetCalculator.cs
@@ -17,18 +17,18 @@ namespace WorkShop22
         internal decimal TotalAmount(DateTime startTime, DateTime endTime)
         {
             var period = new Period(startTime, endTime);
-           
 
             if (period.IsSameMonth())
             {
-                return CalculateBudget(startTime, endTime);
+                return CalculateBudget(period.StartTime, period.EndTime);
             }
-            _result += FirstMonthBudget(startTime);
-            _result += LastMonthBudget(endTime);
-            if (IsOver2Months(startTime, endTime))
+
+            _result += FirstMonthBudget(period.StartTime);
+            _result += LastMonthBudget(period.EndTime);
+            if (IsOver2Months(period.StartTime, period.EndTime))
             {
-                var targetStartTime = new DateTime(startTime.Year, startTime.Month, 1);
-                var targetEndTime = new DateTime(endTime.Year, endTime.Month, 1).AddMonths(-1);
+                var targetStartTime = new DateTime(period.StartTime.Year, period.StartTime.Month, 1);
+                var targetEndTime = new DateTime(period.EndTime.Year, period.EndTime.Month, 1).AddMonths(-1);
                 while (targetStartTime < targetEndTime)
                 {
                     targetStartTime = targetStartTime.AddMonths(1);
@@ -40,15 +40,15 @@ namespace WorkShop22
             }
             return _result;
 
-            DateTime Counter = new DateTime(startTime.Year, startTime.Month, 1);
-            if (IsOver2Months(startTime, endTime))
+            DateTime Counter = new DateTime(period.StartTime.Year, period.StartTime.Month, 1);
+            if (IsOver2Months(period.StartTime, period.EndTime))
             {
                 do
                 {
                     DateTime startTime2 = Counter.AddMonths(1);
                     _result += CalculateBudget(startTime2, Counter.AddMonths(2).AddDays(-1));
                     Counter = Counter.AddMonths(1);
-                } while (Counter.Month != endTime.AddMonths(-1).Month);
+                } while (Counter.Month != period.EndTime.AddMonths(-1).Month);
             }
             return _result;
         }
@@ -80,7 +80,8 @@ namespace WorkShop22
         private static int CalculateBudget(DateTime startTime, DateTime endTime)
         {
             var budgets = _budgetRepository.GetBudgets();
-            var budget= budgets.SingleOrDefault(x => x.YearMonth == startTime.ToString("yyyyMM"))?.Amount ?? 0;
+            var singleOrDefault = budgets.SingleOrDefault(x => x.YearMonth == startTime.ToString("yyyyMM"));
+            var budget= singleOrDefault?.Amount ?? 0;
             return new Period(startTime, endTime).Days() * budget / DateTime.DaysInMonth(startTime.Year, startTime.Month);
         }
 
